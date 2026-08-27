bfe13468a4c9413b5ff724a0316a7196a4cc229e
miyori-knzk <miyu.a_1022@icloud,com>
2026-08-27 14:22:21 +0900

    feat: マイグレーションファイル作成

--- database/migrations/2026_08_27_025727_create_attendance_records_table.php
+++ database/migrations/2026_08_27_025727_create_attendance_records_table.php
@@ -11,15 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('attendance_records', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->tinyInteger('is_admin')->nullable()->comment('管理者は1を入力');
-            $table->rememberToken();
+            $table->foreignId('user_id');
+            $table->date('date');
             $table->timestamps();
+            $table->unique(['user_id', 'date']);
         });
     }
 
@@ -28,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('attendance_records');
     }
 };
--- database/migrations/2026_08_27_050612_create_attendance_statuses_table.php
+++ database/migrations/2026_08_27_050612_create_attendance_statuses_table.php
@@ -11,14 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('attendance_statuses', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->tinyInteger('is_admin')->nullable()->comment('管理者は1を入力');
-            $table->rememberToken();
+            $table->foreignId('attendance_id');
+            $table->tinyInteger('status')->comment('1:出勤中、2:休憩中、3:退勤済');
             $table->timestamps();
         });
     }
@@ -28,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('attendance_statuses');
     }
 };
