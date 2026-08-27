6ce3e22f7766f8dbad149c86d9290ca908c13e9a
miyori-knzk <miyu.a_1022@icloud,com>
2026-08-27 15:08:52 +0900

    fix: マイグレーションファイル修正

--- database/migrations/2026_08_27_025727_create_attendance_table.php
+++ database/migrations/2026_08_27_025727_create_attendance_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('attendance_records', function (Blueprint $table) {
+        Schema::create('attendance', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id');
             $table->date('date');
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('attendance_records');
+        Schema::dropIfExists('attendance');
     }
 };
