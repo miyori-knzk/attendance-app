c80ad00ba590badc4efbd1954c99f49e82e557b2
miyori-knzk <miyu.a_1022@icloud,com>
2026-09-01 21:28:59 +0900

    feat: 申請一覧画面作成

--- app/Models/Application.php
+++ app/Models/Application.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Application extends Model
 {
@@ -15,8 +16,27 @@ class Application extends Model
         'comment',
     ];
 
-    public function attendance(): BelongsTo
+    public function getapprovalStatusAttribute(): string
     {
-        return $this->belongsTo(Attendance::class);
+        return match ($this->status) {
+            1 => '承認待ち',
+            2 => '承認済み',
+            default => '未申請',
+        };
+    }
+
+    public function attendanceRecord(): BelongsTo
+    {
+        return $this->belongsTo(Attendance::class, 'attendance_id', 'id');
+    }
+
+    public function getUserAttribute()
+    {
+        return $this->attendanceRecord->user;
+    }
+
+    public function getApplicationDateAttribute()
+    {
+        return $this->created_at;
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -74,4 +74,15 @@ class User extends Authenticatable
             return '出勤中';
         }
     }
+
+    public function isAdmin()
+    {
+        $role = null;
+
+        if (auth()->user()->is_admin === 1) {
+            $role = 'isAdmin';
+        }
+
+        return $role;
+    }
 }
