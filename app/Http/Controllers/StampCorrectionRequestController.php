140dd9e68798a6291ad2b52b48e67c453cdc2725
miyori-knzk <miyu.a_1022@icloud,com>
2026-09-01 12:11:06 +0900

    feat: 修正ボタンの処理を実装

---
+++ app/Http/Controllers/StampCorrectionRequestController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\AttendanceUpdateRequest;
+
+class StampCorrectionRequestController extends Controller
+{
+    public function store(AttendanceUpdateRequest $reeqest, Attendance $attendance)
+    {
+        dd();
+    }
+}
