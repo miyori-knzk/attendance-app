2587c4613ca8a9b366114f0a2ffa2cd4a9ca6039
miyori-knzk <miyu.a_1022@icloud,com>
2026-09-11 22:22:36 +0900

    コミットテスト

--- lang/ja/validation.php
+++ lang/ja/validation.php
@@ -3,7 +3,7 @@
 return [
     'custom' => [
         'name' => [
-            'required' => 'お名前を入力してください',
+            'required' => 'お名前を入力してださい',
         ],
         'email' => [
             'required' => 'メールアドレスを入力してください',
